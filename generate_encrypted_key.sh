85dfd98dcf56ca5a899b75bc60b8b84640085ecd
Max Gomes <maximiliano.pinto@klarna.com>
2021-11-28 14:25:27 +0100

    Generate keys and update script to build aab

---
+++ generate_encrypted_key.sh
@@ -0,0 +1,1 @@
+java -jar pepk.jar --keystore=sharebook.keystore --output=sharebook.bundle.keystore --alias=sharebook --encryptionkey=eb10fe8f7c7c9df715022017b00c6471f8ba8170b13049a11e6c09ffe3056a104a3bbe4ac5a955f4ba4fe93fc8cef27558a3eb9d2a529a2092761fb833b656cd48b9de6a
